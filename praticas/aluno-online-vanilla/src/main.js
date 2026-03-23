36e25b13f7bbf491caada73ee25ed646bd004eb7
KauanSantos06 <kauanuruana@gmail.com>
2026-03-23 10:50:06 -0300

    Conclui a pratica02

--- praticas/aluno-online-vanilla/src/main.js
+++ praticas/aluno-online-vanilla/src/main.js
@@ -1,1 +1,49 @@
 import './style.css'
+
+const form = document.getElementById("loginForm");
+
+const email = document.getElementById("email");
+const senha = document.getElementById("senha");
+
+const erroEmail = document.getElementById("erroEmail");
+const erroSenha = document.getElementById("erroSenha");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault(); // NÃO recarrega a página
+
+    let valido = true;
+
+    // Validação email
+    if (email.value.trim() === "") {
+        erroEmail.textContent = "O campo de email é obrigatório.";
+        valido = false;
+    } else {
+        erroEmail.textContent = "";
+    }
+
+    // Validação senha
+    if (senha.value.trim() === "") {
+        erroSenha.textContent = "O campo de senha é obrigatório.";
+        valido = false;
+    } else {
+        erroSenha.textContent = "";
+    }
+
+    // Se estiver tudo certo
+    if (valido) {
+        window.location.href = "index.html";
+    }
+});
+
+// Remove erro ao digitar
+email.addEventListener("input", () => {
+    if (email.value.trim() !== "") {
+        erroEmail.textContent = "";
+    }
+});
+
+senha.addEventListener("input", () => {
+    if (senha.value.trim() !== "") {
+        erroSenha.textContent = "";
+    }
+});
