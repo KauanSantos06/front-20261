9cfc7ba922fde73f794a788856c7f3fecfa56798
KauanSantos06 <kauanuruana@gmail.com>
2026-03-30 12:10:33 -0300

    Aula 3

---
+++ aulas/aula03/src/App.jsx
@@ -0,0 +1,58 @@
+function App() {
+
+
+  return <div>
+    <aside>
+      <header>
+        <img src="" alt="" />
+        <h1> Aluno Online </h1>
+      </header>
+      <nav>
+        <ul>
+          <li> Dashboard</li>
+          <li>Notas</li>
+          <li>Faltas</li>
+          <li>Boletos</li>
+          <li>Requerimentos</li>
+          <li>Sair</li>
+        </ul>
+      </nav>
+    </aside>
+    <main>
+      <header>
+        <h1> Olá, Aluno!</h1>
+        <img src="" alt="imagem do avatar" />
+      </header>
+      <h2>Bem-Vindos ao portal do aluno</h2>
+      <section>
+        <article>
+          <h3>Mural de avisos</h3>
+          <ul>
+            <li>Inscrição para o projeto de extensão</li>
+            <li>Eleição para representante de turma</li>
+            <li></li>
+          </ul>
+        </article>
+        <article>
+          <h3> Calendario Acadêmico</h3>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </article>
+        <article>
+          <h3>Minhas diciplinas</h3>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </article>
+      </section>
+    </main>
+
+  </div>
+}
+
+export default App;
