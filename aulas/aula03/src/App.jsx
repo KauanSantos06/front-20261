03209bc6722a2835fe2b17c26e188170fc1b827f
KauanSantos06 <kauanuruana@gmail.com>
2026-04-06 15:47:06 -0300

    Paginas aula03 finalizadas prontas para usar na pratica

--- aulas/aula03/src/App.jsx
+++ aulas/aula03/src/App.jsx
@@ -1,10 +1,11 @@
+import Boletos from "./pages/Boletos";
 import Dashboard from "./pages/Dashboard";
 import Faltas from "./pages/Faltas";
 import Notas from "./pages/Notas";
 import Requerimentos from "./pages/Requerimentos";
 
 function App(){
-  return <Requerimentos />
+  return <Boletos />
 }
 
 export default App;
