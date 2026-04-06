32db6f4f2f128a7371882dbe2b681f7a17a9fc23
KauanSantos06 <kauanuruana@gmail.com>
2026-04-06 16:18:14 -0300

    feat: conclui pratica03. Fecha #13

---
+++ praticas/aluno-online-react/src/pages/Dashboard.jsx
@@ -0,0 +1,22 @@
+import Sidebar from "../components/Sidebar";
+import Topbar from "../components/Topbar";
+import Main from "../components/Main";
+
+function Dashboard(){
+    return(
+        <>
+            <section className="flex h-screen bg-gray-100">
+
+                <Sidebar />
+
+                <main className="flex-1 p-6">
+                        <Topbar titulo="Olá, Aluno!" />      
+                        <Main />
+                </main>
+
+            </section>
+        </>
+    );
+}
+
+export default Dashboard;
