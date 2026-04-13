c75758666f763fc4be71a97579653f22a296d30c
KauanSantos06 <kauanuruana@gmail.com>
2026-04-13 09:30:59 -0300

    feat: conclui pratica01. Fecha #18

--- praticas/aluno-online-react/src/components/Main.jsx
+++ praticas/aluno-online-react/src/components/Main.jsx
@@ -1,16 +1,23 @@
-import Topbar from "./Topbar";
 import Card from "./Card";
 
-function Main() {
+function Main({ titulo, cards }) {
   return (
     <main className="flex-1 p-6 md:p-10">
 
       <h2 className="text-lg md:text-xl font-semibold mb-6">
-        Bem-vindo ao portal do aluno
+        {titulo}
       </h2>
 
-      <Card />
-      
+      <section className="space-y-6">
+        {cards.map((card, index) => (
+          <Card
+            key={index}
+            titulo={card.titulo}
+            conteudo={card.conteudo}
+          />
+        ))}
+      </section>
+
     </main>
   );
 }
--- praticas/aluno-online-react/src/pages/Dashboard.jsx
+++ praticas/aluno-online-react/src/pages/Dashboard.jsx
@@ -4,19 +4,62 @@ import Main from "../components/Main";
 
 function Dashboard(){
     return(
-        <>
-            <section className="flex h-screen bg-gray-100">
+        <section className="flex h-screen bg-gray-100">
 
-                <Sidebar />
+            <Sidebar
+              logo="/src/assets/learn.svg"
+              titulo="Aluno Online"
+              menuItens={[
+                "Dashboard",
+                "Notas",
+                "Faltas",
+                "Boletos",
+                "Requerimentos",
+                "Sair"
+              ]}
+            />
 
-                <main className="flex-1 p-6">
-                        <Topbar titulo="Olá, Aluno!" />      
-                        <Main />
-                </main>
+            <main className="flex-1 p-6">
+                
+                <Topbar 
+                  titulo="Olá, Aluno!"
+                  avatar="/src/assets/user.svg"
+                />
 
-            </section>
-        </>
+                <Main 
+                  titulo="Bem-vindo ao portal do aluno"
+                  cards={[
+                    {
+                      titulo: "Mural de Avisos",
+                      conteudo: [
+                        "Inscrição para o projeto de extensão",
+                        "Eleição para representante de turma"
+                      ]
+                    },
+                    {
+                      titulo: "Calendário Acadêmico",
+                      conteudo: [
+                        "23/02 - Início do período letivo",
+                        "25/04 - Prazo final para a aplicação da P1",
+                        "23/06 - Prazo final para a aplicação da P2",
+                        "04/07 - Fim do périodo letivo"
+                      ]
+                    },
+                                         {
+                      titulo: "Minhas disciplinas",
+                      conteudo: [
+                        "Construção de Front-end",
+                        "Manutenção de Software e Dev ops",
+                        "Business Intelligence e Data Warehousing",
+                        "Disciplina Blackboard"
+                      ]
+                    }
+                  ]}
+                />
+
+            </main>
+
+        </section>
     );
 }
-
 export default Dashboard;
